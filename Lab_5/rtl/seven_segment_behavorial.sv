5a4d8d35358e60a76a07a79f030ae05a6fe2b44e
meh <blah4209211@gmail.com>
2025-02-26 22:39:15 +0500

    clean up

--- Lab_5/rtl/seven_segment_behavorial.sv
+++ Lab_5/rtl/seven_segment_behavorial.sv
@@ -26,23 +26,23 @@ module seven_segment(
     
     always_comb begin
         case (num)
-            4'b0000: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0000001; // 0
-            4'b0001: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b1001111; // 1
-            4'b0010: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0010010; // 2
-            4'b0011: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0000110; // 3
-            4'b0100: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b1001100; // 4
-            4'b0101: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0100100; // 5
-            4'b0110: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0100000; // 6
-            4'b0111: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0001111; // 7
-            4'b1000: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0000000; // 8
-            4'b1001: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0000100; // 9
-            4'b1010: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0001000; // A
-            4'b1011: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b1100000; // B
-            4'b1100: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0110001; // C
-            4'b1101: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b1000010; // D
-            4'b1110: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0110000; // E
-            4'b1111: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0111000; // F
-            default: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b1111111; // Blank
+            4'b0000: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0000001; 
+            4'b0001: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b1001111; 
+            4'b0010: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0010010; 
+            4'b0011: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0000110; 
+            4'b0100: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b1001100; 
+            4'b0101: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0100100; 
+            4'b0110: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0100000; 
+            4'b0111: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0001111; 
+            4'b1000: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0000000; 
+            4'b1001: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0000100; 
+            4'b1010: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0001000; 
+            4'b1011: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b1100000; 
+            4'b1100: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0110001; 
+            4'b1101: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b1000010; 
+            4'b1110: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0110000; 
+            4'b1111: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b0111000; 
+            default: {SegA, SegB, SegC, SegD, SegE, SegF, SegG} = 7'b1111111; 
         endcase
     end
     
