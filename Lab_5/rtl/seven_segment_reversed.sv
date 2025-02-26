d3d165bfe5cfb18bafe78334eb5b3befd0b63ea6
meh <blah4209211@gmail.com>
2025-02-26 20:27:11 +0500

    add reversed file

---
+++ Lab_5/rtl/seven_segment_reversed.sv
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+
+
+
+module seven_segment(
+    input [3:0] num,
+    input [2:0] sel,
+    input DP_IN,
+    output SegA, SegB, SegC, SegD, SegE, SegF, SegG, DP,
+    output [7:0] AN
+    );
+
+    assign DP = DP_IN;
+
+
+    assign AN[0] = ~(~sel[0] & ~sel[1] & ~sel[2]);
+    assign AN[1] = ~(~sel[0] & ~sel[1] & sel[2]);
+    assign AN[2] = ~(~sel[0] & sel[1] & ~sel[2]);
+    assign AN[3] = ~(~sel[0] & sel[1] & sel[2]);
+    assign AN[4] = ~(sel[0] & ~sel[1] & ~sel[2]);
+    assign AN[5] = ~(sel[0] & ~sel[1] & sel[2]);
+    assign AN[6] = ~(sel[0] & sel[1] & ~sel[2]);
+    assign AN[7] = ~(sel[0] & sel[1] & sel[2]);
+
+
+    assign SegA = (~num[3] & num[2] & ~num[1] & ~num[0]) |
+                  (~num[3] & ~num[2] & ~num[1] & num[0]) |
+                  (num[3] & num[2] & ~num[1] & num[0]) |
+                  (num[3] & ~num[2] & num[1] & num[0]);
+
+    assign SegB = (~num[3] & num[2] & ~num[1] & num[0]) |
+                  (num[2] & num[1] & ~num[0]) |
+                  (num[3] & num[1] & num[0]) |
+                  (num[3] & num[2] & ~num[0]);
+
+    assign SegC = (num[3] & num[2] & num[1]) |
+                  (num[3] & num[2] & ~num[0]) |
+                  (~num[3] & ~num[2] & num[1] & ~num[0]);
+
+    assign SegD = (num[2] & num[1] & num[0]) |
+                  (num[3] & ~num[2] & num[1] & ~num[0]) |
+                  (~num[3] & num[2] & ~num[1] & ~num[0]) |
+                  (~num[3] & ~num[2] & ~num[1] & num[0]);
+
+    assign SegE = (~num[3] & num[0]) |
+                  (~num[3] & num[2] & ~num[1]) |
+                  (~num[2] & ~num[1] & num[0]);
+
+    assign SegF = (~num[3] & ~num[2] & num[0]) |
+                  (~num[3] & ~num[2] & num[1]) |
+                  (~num[3] & num[1] & num[0]) |
+                  (num[3] & num[2] & ~num[1] & num[0]);
+
+    assign SegG = (~num[3] & ~num[2] & ~num[1]) |
+                  (~num[3] & num[2] & num[1] & num[0]) |
+                  (num[3] & num[2] & ~num[1] & ~num[0]);
+
+
+endmodule
